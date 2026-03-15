ceb5cd38bcdf0a04ccfc9373ea97d6f1bc4231e9
Divyansh Garg <divyanshgarg955@gmail.com>
2026-03-15 16:17:16 +0530

    Extracted the Job position/role from the same HTML and parse it through txt file for further processing
    Signed-off-by: Divyansh Garg <divyanshgarg955@gmail.com>

--- JobUpdates/parseHTMLtoTxt.py
+++ JobUpdates/parseHTMLtoTxt.py
@@ -16,16 +16,19 @@ def extract_company_names(html_file):
     # Find all matching elements
     company_names = soup.find_all("div", class_="artdeco-entity-lockup__subtitle")
     job_locations = soup.find_all("div", class_="artdeco-entity-lockup__caption")
+    job_positions = soup.find_all("div", class_="artdeco-entity-lockup__title")
 
     extracted_data = []
 
-    for company, location in zip(company_names, job_locations):
+    for company, location, position in zip(company_names, job_locations, job_positions):
         company_name = company.get_text(strip=True)
         job_location = location.get_text(strip=True)
+        job_role = position.find("span", attrs={"aria-hidden": "true"}).get_text(strip=True)
 
         extracted_data.append({
             "company": company_name,
-            "location": job_location
+            "location": job_location,
+            "role": job_role
         })
 
     # Determine txt file path
@@ -35,6 +38,6 @@ def extract_company_names(html_file):
     # Append data to file
     with open(txt_path, "a", encoding="utf-8") as f:
         for item in extracted_data:
-            f.write(f"{item['company']} | {item['location']}\n")
+            f.write(f"{item['company']} | {item['location']} | {item['role']}\n")
 
     print(f"{len(extracted_data)} records saved to {txt_path}")
